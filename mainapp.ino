a5b5124c6e8554db39c66e9be967beb241ea42e3
zeinaa19 <ze2300849@tkh.edu.eg>
2024-11-13 16:52:23 +0200

    Update mainapp.ino

--- mainapp.ino
+++ mainapp.ino
@@ -13,7 +13,7 @@ int main (void) { // this is the bedining of the main code
     unsigned short lower=0;  // defines lower as a variable
     unsigned char buffer[6]; //define th buffer as an unsigned charecter that holds only positive values to store 6 elements 
     while (1) {  // using while(1) as an infinite continuous loop 
-        _delay_ms(200);  // setting delay as 2oo to ensure data runs smoothly 
+        _delay_ms(200);  // setting a delay too to ensure data runs smoothly 
 
         adcsensor=Adc_ReadChannel(1); //reads the analog value from the sensor from channel A1
         temperature=adcsensor*(5.0/1024.0)*100;  //this formula is used to calculate the temperature from the sensor as its output is in millivolts 
@@ -24,20 +24,20 @@ int main (void) { // this is the bedining of the main code
             lower += 5; // if the button is pressed the lower limit increases by 5 
             _delay_ms(200); // adding a delay of 200 milliseconds to ensure the code runs smoothly
         } 
-        if (((PINB >> PB5)&1)==0) { // if the button is pressed the upper limit is increased
-            upper += 5; 
-            _delay_ms(200);
+        if (((PINB >> PB5)&1)==0) { //this if contition is used to check if the button to check if the button is pressed or not
+            upper += 5; // if the button is pressed the upper limit increases by 5
+            _delay_ms(200); // adding a dely of 200 milliseconds to ensure that the code runs smoothly
         } 
         
-        LCD_Clear(); 
+        LCD_Clear(); //this comand is to clear the LCD
 
         
         LCD_Command(0x80); 
-        LCD_String("Temp:"); 
-        LCD_String(buffer); 
-        LCD_String("C"); 
+        LCD_String("Temp:"); // displays the word "temp"
+        LCD_String(buffer); // displays value stores in the buffer
+        LCD_String("C"); // displays degree celsius 
 
-        LCD_Command(0xC0); 
+        LCD_Command(0xC0); // this command is sued to shift the second row of the lcd
         LCD_String("L:");
         itoa(lower,buffer,10); 
         LCD_String(buffer); 
@@ -46,17 +46,17 @@ int main (void) { // this is the bedining of the main code
         LCD_String(buffer);
 
        
-        if (temperature>=lower&&temperature<upper) { 
-            PORTD |= (1<<PD2);  
+        if (temperature>=lower&&temperature<upper) { //this checks if the tempereture sensor value is inside the range or not
+            PORTD |= (1<<PD2);  // the led turns on if its inside the range
             PORTD &= ~(1<<PD0); 
             LCD_Command(0x8E);    
-            LCD_String("OK");     
+            LCD_String("OK");     //displays "ok" if the value is in the range
         } 
         else { 
-            PORTD |= (1<<PD0);  
+            PORTD |= (1<<PD0);  //turns off the led if its outside the range 
             PORTD &= ~(1<<PD2); 
             LCD_Command(0x8D);    
-            LCD_String("Nok");    
+            LCD_String("Nok");    //displays "nok" if the value is out of range
         }
     }
 
